209de1b0edf618160bcdea8d612ff61039fb0a9a
unknown <o.sabodash@mail.ru>
2018-09-12 09:27:47 +0300

    task

--- src/index.js
+++ src/index.js
@@ -1,30 +1,44 @@
 class Sorter {
   constructor() {
-    // your implementation
+    this.arrayNumbers = [];
+    this.sortFunction = function(a, b){return a - b};
   }
 
   add(element) {
-    // your implementation
+    this.arrayNumbers.push(element);
   }
 
   at(index) {
-    // your implementation
+    return this.arrayNumbers[index];
   }
 
   get length() {
-    // your implementation
+    return this.arrayNumbers.length;
   }
 
   toArray() {
-    // your implementation
+    return this.arrayNumbers;
   }
 
   sort(indices) {
-    // your implementation
+    
+    indices.sort(function(a, b){ return a - b});
+    var arr = [];
+
+    for (let i = 0; i < indices.length; i++) {
+        arr.push(this.arrayNumbers[indices[i]]);
+    };
+    arr.sort(this.sortFunction);
+
+    for (let i = 0; i < indices.length; i++) {
+        this.arrayNumbers[indices[i]] = arr[i];
+    };
+
   }
 
   setComparator(compareFunction) {
-    // your implementation
+     this.sortFunction = compareFunction;
+
   }
 }
 
